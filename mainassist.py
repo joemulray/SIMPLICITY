f51c560f09cf44762f90bf47426f296a87cfe71e
Joseph Mulray <joe.mulray@gmail.com>
2017-10-21 12:00:02 +0100

    updating main file

--- mainassist.py
+++ mainassist.py
@@ -11,6 +11,33 @@ ask = Ask(app, "/")
 def homepage():
     return "Hello"
 
+@ask.launch
+def start():
+    welcome_message = 'Hello there, Welcome to the HSBC Alexa App. \
+    Would you like to hear your balance.'
+    return question(welcome_message)
+
+@ask.intent("YesIntent")
+def search():
+	return "Your balance is $120.00"
+
+@ask.intent("NoIntent")
+def no_intent():
+	return "I'm sorry to hear that."
+
+
+
+
+#Class for information about user if needed
+class Person:
+	def __init__(self):
+		self.name = "User1"
+		self.email = "user1@example.com"
+		self.bankid = "12345"
+		self.passwd = "password123"
+		self.securityq = "December"
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
