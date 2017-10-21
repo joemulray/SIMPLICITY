0e3e10d39f0234033c2280152502531a0d78d8d8
Joseph Mulray <joe.mulray@gmail.com>
2017-10-21 15:00:27 +0100

    simple update :school_satchel:

--- mainassist.py
+++ mainassist.py
@@ -9,13 +9,15 @@ ask = Ask(app, "/")
 
 @app.route('/')
 def homepage():
-    return "Hello"
+    return "HSBC Alexa Homepage"
 
 @ask.launch
 def start():
-    welcome_message = 'Hello there, Welcome to the HSBC Alexa App. \
-    Would you like to hear your balance.'
+    welcome_message = 'Hello there, Welcome to the HSBC Alexa App\
+    .. What can we help you with'
     return question(welcome_message)
+
+
 
 @ask.intent("YesIntent")
 def yes_intent():
@@ -25,10 +27,19 @@ def yes_intent():
 def no_intent():
 	return statement("I'm sorry to hear that.")
 
+@ask.intent('AccountIntent')
+def account_intent():
+
+	return statement("Your Account number is {}".format(Person().balance))
+
+@ask.intent('AccountBalance')
+def balance():
+
+	return statement("Your Account balance is ${}".format(Person().balance))
+
+
 
 #Add ability to query last months bills
-
-
 #Class for information about user if needed
 class Person:
 	def __init__(self):
@@ -37,6 +48,7 @@ class Person:
 		self.bankid = "12345"
 		self.passwd = "password123"
 		self.securityq = "December"
+		self.balance = 124000
 
 
 if __name__ == '__main__':
